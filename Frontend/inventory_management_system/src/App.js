c7cc13ab64a46f120adce27595dd6f86a4fac06c
Pardeep swami <105664755+Swami5911@users.noreply.github.com>
2025-01-17 09:37:01 +0530

    Add files via upload

--- Frontend/inventory_management_system/src/App.js
+++ Frontend/inventory_management_system/src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
-import InsertProduct from './components/InsertProduct'
+import InsertProduct from './components/InsertProduct';
 import UpdateProduct from './components/UpdateProduct';
 import About from './components/About';
+import Inventory from './components/Inventory'; // Import the Inventory component
 
 import {
   BrowserRouter as Router,
@@ -12,28 +13,21 @@ import {
   Route
 } from 'react-router-dom';
 
-
-
-
 function App() {
   return (
-    <div className="App">
-      <Navbar title="IMS" about="About"></Navbar>
-
-      <Router>
+    <Router> {/* Ensure Router wraps the entire application */}
+      <div className="App">
+        <Navbar title="ShopDesk" about="About" />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/insertproduct" element={<InsertProduct />} />
           <Route path="/updateproduct/:id" element={<UpdateProduct />} />
           <Route path="/about" element={<About />} />
-
+          <Route path="/inventory" element={<Inventory />} /> {/* Add Inventory route */}
         </Routes>
-
-      </Router>
-
-
-    </div>
+      </div>
+    </Router>
   );
 }
 
